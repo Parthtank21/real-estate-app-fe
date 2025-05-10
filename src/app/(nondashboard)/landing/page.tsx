d04bf6ff51d10e314acf2a016e5ca1bb81d3d15e
Parth Tank <parthtank301@gmail.com>
2025-05-10 15:25:32 +0530

    ui: completed features section

--- src/app/(nondashboard)/landing/page.tsx
+++ src/app/(nondashboard)/landing/page.tsx
@@ -1,10 +1,12 @@
 import React from "react";
 import HeroSection from "./hero-section";
+import FeaturesSection from "./features-section";
 
 export default function Landing() {
   return (
     <div>
       <HeroSection />
+      <FeaturesSection />
     </div>
   );
 }
