9d73345c0b99f806015bd3ef06b4945cbc8b670d
Parth Tank <parthtank301@gmail.com>
2025-05-10 15:44:45 +0530

    ui: completed discover section

--- src/app/(nondashboard)/landing/page.tsx
+++ src/app/(nondashboard)/landing/page.tsx
@@ -1,12 +1,14 @@
 import React from "react";
 import HeroSection from "./hero-section";
 import FeaturesSection from "./features-section";
+import DiscoverSection from "./discover-section";
 
 export default function Landing() {
   return (
     <div>
       <HeroSection />
       <FeaturesSection />
+      <DiscoverSection />
     </div>
   );
 }
