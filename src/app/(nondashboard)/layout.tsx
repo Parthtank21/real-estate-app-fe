d3d8bb18202891e964cc6a47817da5f91442023c
Parth Tank <parthtank301@gmail.com>
2025-06-02 21:48:06 +0530

    feat: completed sidebar & route check for different user-roles

--- src/app/(nondashboard)/layout.tsx
+++ src/app/(nondashboard)/layout.tsx
@@ -1,12 +1,33 @@
 "use client";
 
-import React from "react";
-import { NAVBAR_HEIGHT } from "@/lib/constants";
+import React, { useEffect, useState } from "react";
+import { usePathname, useRouter } from "next/navigation";
 import Navbar from "@/components/navbar";
+import { NAVBAR_HEIGHT } from "@/lib/constants";
 import { useGetAuthUserQuery } from "@/state/api";
 
 export default function Layout({ children }: { children: React.ReactNode }) {
-  const { data: authUser } = useGetAuthUserQuery();
+  const router = useRouter();
+  const pathname = usePathname();
+  const [isLoading, setIsLoading] = useState(true);
+  const { data: authUser, isLoading: authLoading } = useGetAuthUserQuery();
+
+  useEffect(() => {
+    if (authUser) {
+      const userRole = authUser.userRole?.toLowerCase();
+
+      if (
+        (userRole === "manager" && pathname.startsWith("/search")) ||
+        (userRole === "manager" && pathname === "/")
+      ) {
+        router.push("/managers/properties", { scroll: false });
+      } else {
+        setIsLoading(false);
+      }
+    }
+  }, [authUser, router, pathname]);
+
+  if (authLoading || isLoading) return <>Loading...</>;
 
   return (
     <div className="h-full w-full">
---
+++ src/components/AppSidebar.tsx
@@ -0,0 +1,136 @@
+"use client";
+
+import React from "react";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+import {
+  Building,
+  FileText,
+  Heart,
+  Home,
+  Menu,
+  Settings,
+  X,
+} from "lucide-react";
+import { cn } from "@/lib/utils";
+import { NAVBAR_HEIGHT } from "@/lib/constants";
+import {
+  Sidebar,
+  SidebarContent,
+  SidebarHeader,
+  SidebarMenu,
+  SidebarMenuButton,
+  SidebarMenuItem,
+  useSidebar,
+} from "./ui/sidebar";
+
+export default function AppSidebar({ userType }: AppSidebarProps) {
+  const pathname = usePathname();
+  const { toggleSidebar, open } = useSidebar();
+
+  const navLinks =
+    userType === "manager"
+      ? [
+          { icon: Building, lable: "Properties", href: "/managers/properties" },
+          {
+            icon: FileText,
+            lable: "Applications",
+            href: "/managers/applications",
+          },
+          { icon: Settings, lable: "Settings", href: "/managers/settings" },
+        ]
+      : [
+          { icon: Heart, lable: "Favorites", href: "/tenants/favorites" },
+          {
+            icon: FileText,
+            lable: "Applications",
+            href: "/tenants/applications",
+          },
+          { icon: Home, lable: "Residenses", href: "/tenants/residenses" },
+          { icon: Settings, lable: "Settings", href: "/tenants/settings" },
+        ];
+
+  return (
+    <Sidebar
+      collapsible="icon"
+      className="fixed left-0 bg-white shadow-lg"
+      style={{
+        top: `${NAVBAR_HEIGHT}px`,
+        height: `calc(100vh - ${NAVBAR_HEIGHT}px)`,
+      }}
+    >
+      <SidebarHeader>
+        <SidebarMenu>
+          <SidebarMenuItem>
+            <div
+              className={cn(
+                "flex min-h-[56px] w-full items-center pt-3 mb-3",
+                open ? "justify-between px-6" : "justify-center"
+              )}
+            >
+              {open ? (
+                <>
+                  <h1 className="text-xl font-bold text-gray-800">
+                    {userType === "manager" ? "Manager View" : "Renter View"}
+                  </h1>
+                  <button
+                    className="hover:bg-gray-100 p-2 rounded-md"
+                    onClick={() => toggleSidebar()}
+                  >
+                    <X className="h-6 w-6 text-gray-600" />
+                  </button>
+                </>
+              ) : (
+                <>
+                  <button
+                    className="hover:bg-gray-100 p-2 rounded-md"
+                    onClick={() => toggleSidebar()}
+                  >
+                    <Menu className="h-6 w-6 text-gray-600" />
+                  </button>
+                </>
+              )}
+            </div>
+          </SidebarMenuItem>
+        </SidebarMenu>
+      </SidebarHeader>
+      <SidebarContent>
+        <SidebarMenu>
+          {navLinks.map((link) => {
+            const isActive = pathname === link.href;
+
+            return (
+              <SidebarMenuItem key={link.href}>
+                <SidebarMenuButton
+                  asChild
+                  className={cn(
+                    "flex items-center px-7 py-7",
+                    isActive
+                      ? "text-gray-100"
+                      : "text-gray-600 hover:bg-gray-100",
+                    open ? "text-blue-600" : "ml-[5px]"
+                  )}
+                >
+                  <Link href={link.href} className="w-full" scroll={false}>
+                    <link.icon
+                      className={`h-5 w-5 ${
+                        isActive ? "text-blue-600" : "text-gray-600"
+                      }`}
+                    />
+                    <span
+                      className={`font-medium ${
+                        isActive ? "text-blue-600" : "text-gray-600"
+                      }`}
+                    >
+                      {link.lable}
+                    </span>
+                  </Link>
+                </SidebarMenuButton>
+              </SidebarMenuItem>
+            );
+          })}
+        </SidebarMenu>
+      </SidebarContent>
+    </Sidebar>
+  );
+}
