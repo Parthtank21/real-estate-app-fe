2f50c03e31a4840db43b500819e53df3e129615c
Parth Tank <parthtank301@gmail.com>
2025-06-06 17:54:48 +0530

    fix: minor navbar fix

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -38,7 +38,7 @@ export default function Navbar() {
       className="fixed top-0 left-0 w-full z-50 shadow-xl"
       style={{ height: `${NAVBAR_HEIGHT}px` }}
     >
-      <div className="flex justify-between items-center w-full py-3 px-8 bg-primary-700 text-white">
+      <div className="h-full flex justify-between items-center w-full py-3 px-8 bg-primary-700 text-white">
         <div className="flex items-center gap-4 md:gap-6">
           {isDashboard && (
             <div className="md:hidden">
