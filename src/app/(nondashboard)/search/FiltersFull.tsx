d0ac4c4b8d17dfb1a01246f6ef9e37609d5e2464
Parth Tank <parthtank301@gmail.com>
2025-06-15 12:36:14 +0530

    feat: completed location search function

--- src/app/(nondashboard)/search/FiltersFull.tsx
+++ src/app/(nondashboard)/search/FiltersFull.tsx
@@ -52,6 +52,29 @@ export default function FiltersFull() {
     }));
   };
 
+  const handleLocationSearch = async () => {
+    try {
+      const response = await fetch(
+        `https://api.mapbox.com/geocoding/v5/mapbox.places/${encodeURIComponent(
+          localFilters.location
+        )}.json?access_token=${
+          process.env.NEXT_PUBLIC_MAPBOX_ACCESS_TOKEN
+        }&fuzzyMatch=true`
+      );
+      const data = await response.json();
+      if (data.features && data.features.length > 0) {
+        const [lng, lat] = data.features[0].center;
+
+        setLocalFilters((prev) => ({
+          ...prev,
+          coordinates: [lng, lat],
+        }));
+      }
+    } catch (error) {
+      console.error("Error searching location:", error);
+    }
+  };
+
   const handleSubmit = () => {
     dispatch(setFilters(localFilters));
     updateUrl(localFilters);
@@ -83,7 +106,7 @@ export default function FiltersFull() {
               className="rounded-l-xl rounded-r-none border-r-0"
             />
             <Button
-              //   onClick={handleLocationSearch}
+              onClick={handleLocationSearch}
               className="rounded-r-xl rounded-l-none border-l-none border-black shadow-none border hover:bg-primary-700 hover:text-primary-50"
             >
               <Search className="w-4 h-4" />
